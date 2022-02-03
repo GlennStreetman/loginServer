ba7af0318e23b8ea09494311c8ebaaf3048c9744
Glenn Slear <glennstreetman@gmail.com>
2022-02-02 16:25:10 -0800

    implement shared components, add routing, add warnings

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,98 +1,79 @@
-import React from "react";
-import Topper from "../components/topper";
 import { useState, useEffect } from "react";
-import Image from "next/image";
+import ButtonStandard from "../components/buttonStandard";
+import { SiGithub } from "react-icons/si";
+import { FcGoogle } from "react-icons/fc";
+import ActiveLogo from "../components/activeLogo";
+import Link from "next/link";
+import LabeledInput from "../components/labeledInput";
+import { emailIsValid, passwordIsValid } from "../functions/stringValidators";
 
 function Login() {
     const [email, setEmail] = useState("");
-    const [emailHelpStyling, setEmailHelpStyling] = useState("absolute text-accent bg-primary  rounded-lg");
     const [password, setPassword] = useState("");
-    const [passwordHelpStyling, setPasswordHelpStyling] = useState("absolute text-accent bg-primary  rounded-lg");
+    const [serverMessage, setServerMessage] = useState("");
+    const [emailHelp, setEmailHelp] = useState<boolean | string>(false);
+    const [passwordHelp, setPasswordHelp] = useState<boolean | string>(false);
 
-    // useEffect(() => {
-    //     setEmail("_");
-    //     setPassword("_");
-    //     setTimeout(() => {
-    //         setEmail("");
-    //         setPassword("");
-    //     }, 1);
-    // }, []);
-
-    function helperFocus(helperText: string, helperStyling: Function) {
-        //on focus, move helper text to upper left side of input form, set zIndex to bring it above outline.
-        helperStyling("absolute bottom-8 left-4 z-2  text-accent bg-primary rounded-lg");
-    }
-
-    function helperBlurr(fieldValue: string, helperStyling: Function) {
-        //on focus, move helper text to upper left side of input form, set zIndex to bring it above outline.
-        if (fieldValue === "") {
-            helperStyling("absolute text-accent bg-primary p-1 rounded-lg");
-        }
-    }
-
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>, setText: Function) {
+    const checkLogin = async (e) => {
         e.preventDefault();
-        setText(e.target.value);
-    }
+        console.log("processing request:", email, password);
+
+        const checkEmail = emailIsValid(email);
+        const checkPassword = passwordIsValid(password);
+
+        if (checkEmail && checkPassword) {
+            setEmailHelp(false);
+            setPasswordHelp(false);
+
+            const res = await fetch("/api/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email: email, password: password }),
+            });
+            const data = await res.json();
+            console.log(data);
+            setServerMessage("id: " + data.id + " EM: " + data.email + " pw: " + data.password);
+        } else {
+            !checkEmail ? setEmailHelp("Enter Valid Email Address") : setEmailHelp(false);
+            !checkPassword ? setPasswordHelp("Check Password") : setPasswordHelp(false);
+        }
+    };
 
     return (
-        <div className="grid grid-cols-12 mt-12 mx-1 min-h-screen bg-primary">
-            <Topper />
-            <div className="col-span-0 sm:col-span-2 md:col-span-3 lg:col-span-4 outline-red-500 "></div>
+        <div className="grid grid-cols-12 bg-primary mx-1">
+            <div className="col-span-0 sm:col-span-2 md:col-span-3 lg:col-span-4 "></div>
             <div className="col-span-12 outline-4 border-2 rounded-md sm:col-span-8 md:col-span-6 lg:col-span-4  p-2 shadow-md">
-                <div className="text-secondary flex justify-center">
-                    <Image src="/gstreet2.png" alt="logo" height="250px" width="250px" />
-                </div>
-                <form className="flex flex-col gap-4" autoComplete="off">
-                    <div
-                        className="relative rounded-md border-2 p-2 text-primary"
-                        onFocus={() => helperFocus("Email", setEmailHelpStyling)}
-                        onBlur={() => helperBlurr(email, setEmailHelpStyling)}
-                    >
-                        <label
-                            htmlFor="loginEmailField"
-                            className={emailHelpStyling}
-                            onClick={() => {
-                                document.getElementById("loginEmailField")?.focus();
-                            }}
-                        >
-                            Email
-                        </label>
-                        <input
-                            autoComplete="off"
-                            className="bg-primary outline-none w-full "
-                            id="loginEmailField"
-                            type="text"
-                            required
-                            value={email}
-                            onChange={(e) => handleChange(e, setEmail)}
-                        />
-                    </div>
-                    <div
-                        className="relative border-2 rounded-md p-2"
-                        onFocus={() => helperFocus("Password", setPasswordHelpStyling)}
-                        onBlur={() => helperBlurr(password, setPasswordHelpStyling)}
-                    >
-                        <label
-                            htmlFor="passwordFiel"
-                            className={passwordHelpStyling}
-                            onClick={() => {
-                                document.getElementById("passwordFiel")?.focus();
-                            }}
-                        >
-                            Password
-                        </label>
-                        <input
-                            autoComplete="off"
-                            className="bg-primary outline-none w-full"
-                            id="passwordFiel"
-                            type="password"
-                            required
-                            value={password}
-                            onChange={(e) => handleChange(e, setPassword)}
-                        />
+                <ActiveLogo />
+                <form id="loginForm" className="flex flex-col gap-4 pb-3 justify-center" autoComplete="off">
+                    <LabeledInput id="emailLabel" label="Email" value={email} onClickCallback={setEmail} helperText={emailHelp} />
+                    <LabeledInput
+                        id="passwordLabel"
+                        label="Password"
+                        value={password}
+                        onClickCallback={setPassword}
+                        helperText={passwordHelp}
+                        fieldType={"password"}
+                    />
+                    <div className="flex flex-row justify-center">
+                        <div className="grow flex justify-center gap-2">
+                            <Link href="/register" passHref={true}>
+                                <ButtonStandard onClick={() => {}}>Register</ButtonStandard>
+                            </Link>
+                            <ButtonStandard type="submit" form="loginForm" onClick={checkLogin}>
+                                LOGIN
+                            </ButtonStandard>
+                        </div>
                     </div>
                 </form>
+                <div className="flex flex-row gap-2 justify-center">
+                    <ButtonStandard onClick={() => {}}>
+                        <SiGithub className="text-xl"></SiGithub>Github Login
+                    </ButtonStandard>
+                    <ButtonStandard onClick={() => {}}>
+                        <FcGoogle className="text-xl"></FcGoogle>Google Login
+                    </ButtonStandard>
+                </div>
+                {serverMessage}
             </div>
             <div className="col-span-0 sm:col-span-2 md:col-span-3 lg:col-span-4 outline-red-500"></div>
         </div>
@@ -100,11 +81,3 @@ function Login() {
 }
 
 export default Login;
-
-//back button
-//banner
-//userName
-//password
-//submit
-//forgot login
-//register
