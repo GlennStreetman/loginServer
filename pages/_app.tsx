86dc4e74f1791affd9c54cdc2c1a58d153939c85
Glenn Slear <glennstreetman@gmail.com>
2022-02-16 17:45:52 -0800

    add session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import UserPrefs from "../components/userPrefs";
 import { useEffect } from "react";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
@@ -20,10 +21,31 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     return (
         <>
-            <UserPrefs />
-            <div className="font-body pt-7 bg-primary min-h-screen">
-                <Component {...pageProps} />
-            </div>
+            <SessionProvider
+                // Provider options are not required but can be useful in situations where
+                // you have a short session maxAge time. Shown here with default values.
+                // @ts-ignore
+                options={{
+                    // Stale Time controls how often the useSession in the client should
+                    // contact the server to sync the session state. Value in seconds.
+                    // e.g.
+                    // * 0  - Disabled (always use cache value)
+                    // * 60 - Sync session state with server if it's older than 60 seconds
+                    staleTime: 0,
+                    // Refetch Interval tells windows / tabs that are signed in to keep sending
+                    // a keep alive request (which extends the current session expiry) to
+                    // prevent sessions in open windows from expiring. Value in seconds.
+                    //
+                    // Note: If a session has expired when keep alive is triggered, all open
+                    // windows / tabs will be updated to reflect the user is signed out.
+                    refetchInterval: 0,
+                }}
+            >
+                <UserPrefs />
+                <div className="font-body pt-7 bg-primary min-h-screen">
+                    <Component {...pageProps} />
+                </div>
+            </SessionProvider>
         </>
     );
 }
