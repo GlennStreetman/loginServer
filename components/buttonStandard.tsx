45ddec64434efbf9615678d8b8fbf71cfc3d68c5
Glenn Slear <glennstreetman@gmail.com>
2022-02-16 17:42:47 -0800

    fix spacing on className

--- components/buttonStandard.tsx
+++ components/buttonStandard.tsx
@@ -17,7 +17,7 @@ function buttonStandard(p: props) {
     if (p.form) optionalProps["form"] = p.form;
     return (
         <button
-            className=" flex font-bold uppercase  text-primary hover:text-accent rounded-md active:bg-strong gap-1 border-2 p-2 hover:bg-secondary"
+            className=" flex font-bold uppercase text-primary hover:text-accent rounded-md active:bg-strong gap-1 border-2 p-2 hover:bg-secondary"
             onClick={(e) => {
                 p.onClick(e);
             }}
