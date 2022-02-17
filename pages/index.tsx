89701b63af017af69be73c8a6f14eebf91c56b00
Glenn Slear <glennstreetman@gmail.com>
2022-02-17 11:27:25 -0800

    remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from "next";
 import { useState, useEffect } from "react";
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import LoginHeader from "./../components/loginHeader";
-import { users } from "@prisma/client";
 
 interface user {
     email?: string;
