a280ba7d6d380cc94daa3ac9bf7c2d4cc8064188
Glenn Slear <glennstreetman@gmail.com>
2022-02-16 17:48:51 -0800

    added login providers

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,79 +2,121 @@ import { useState, useEffect } from "react";
 import ButtonStandard from "../components/buttonStandard";
 import { SiGithub } from "react-icons/si";
 import { FcGoogle } from "react-icons/fc";
+import { AiOutlineMail } from "react-icons/ai";
 import ActiveLogo from "../components/activeLogo";
-import Link from "next/link";
 import LabeledInput from "../components/labeledInput";
-import { emailIsValid, passwordIsValid } from "../functions/stringValidators";
+import { emailIsValid } from "../functions/stringValidators";
+import { getProviders, signIn, getSession, getCsrfToken } from "next-auth/react";
+import { useRouter } from "next/router";
+import { PrismaClient } from "@prisma/client";
+import { useSession } from "next-auth/react";
 
-function Login() {
+Login.getInitialProps = async (context) => {
+    //if already signed in redirect to homepage.
+    const { req, res } = context;
+    const session = await getSession({ req });
+    const userEmail = session?.userEmail ? session.userEmail : undefined;
+    if (session && res) {
+        res.writeHead(302, {
+            Location: "/",
+        });
+        res.end();
+        return;
+    }
+    return {
+        email: userEmail,
+        providers: await getProviders(),
+        csrfToken: await getCsrfToken(context), //email signin only
+    };
+};
+
+interface props {
+    email: string | undefined;
+    providers: any;
+    csrfToken: any;
+}
+
+function Login(p: props) {
+    const router = useRouter();
+
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const [serverMessage, setServerMessage] = useState("");
     const [emailHelp, setEmailHelp] = useState<boolean | string>(false);
-    const [passwordHelp, setPasswordHelp] = useState<boolean | string>(false);
 
-    const checkLogin = async (e) => {
+    useEffect(() => {
+        if (router.query.error === "OAuthAccountNotLinked") {
+            setServerMessage(
+                'Associated login email already used with previous login provider. Use "Login with email" or select previously used login provider.'
+            );
+        }
+    }, []);
+
+    const emailLogin = async (e) => {
         e.preventDefault();
-        console.log("processing request:", email, password);
 
         const checkEmail = emailIsValid(email);
-        const checkPassword = passwordIsValid(password);
-
-        if (checkEmail && checkPassword) {
+        if (checkEmail) {
             setEmailHelp(false);
-            setPasswordHelp(false);
-
-            const res = await fetch("/api/login", {
+            fetch("/api/auth/signin/email", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ email: email, password: password }),
+                body: JSON.stringify({ email: email, csrfToken: p.csrfToken }),
             });
-            const data = await res.json();
-            console.log(data);
-            setServerMessage("id: " + data.id + " EM: " + data.email + " pw: " + data.password);
+            setServerMessage(`Check provided email account for login instructions.`);
         } else {
             !checkEmail ? setEmailHelp("Enter Valid Email Address") : setEmailHelp(false);
-            !checkPassword ? setPasswordHelp("Check Password") : setPasswordHelp(false);
         }
     };
 
     return (
         <div className="grid grid-cols-12 bg-primary mx-1">
-            <div className="col-span-0 sm:col-span-2 md:col-span-3 lg:col-span-4 "></div>
-            <div className="col-span-12 outline-4 border-2 rounded-md sm:col-span-8 md:col-span-6 lg:col-span-4  p-2 shadow-md">
-                <ActiveLogo />
-                <form id="loginForm" className="flex flex-col gap-4 pb-3 justify-center" autoComplete="off">
-                    <LabeledInput id="emailLabel" label="Email" value={email} onClickCallback={setEmail} helperText={emailHelp} />
-                    <LabeledInput
-                        id="passwordLabel"
-                        label="Password"
-                        value={password}
-                        onClickCallback={setPassword}
-                        helperText={passwordHelp}
-                        fieldType={"password"}
-                    />
-                    <div className="flex flex-row justify-center">
-                        <div className="grow flex justify-center gap-2">
-                            <Link href="/register" passHref={true}>
-                                <ButtonStandard onClick={() => {}}>Register</ButtonStandard>
-                            </Link>
-                            <ButtonStandard type="submit" form="loginForm" onClick={checkLogin}>
-                                LOGIN
-                            </ButtonStandard>
+            <div className=" col-span-0 sm:col-span-2 md:col-span-3 lg:col-span-4 "></div>
+            {/* <div className="flex flex-col"> */}
+            <div className="col-span-12  sm:col-span-8 md:col-span-6 lg:col-span-4 ">
+                <div className="outline-4 border-2 rounded-md  p-2 shadow-md mb-4">
+                    <ActiveLogo />
+                    <form id="loginForm" className="flex flex-col gap-2 justify-center" autoComplete="off">
+                        <LabeledInput id="emailLabel" label="Email" value={email} onClickCallback={setEmail} helperText={emailHelp} />
+                        <div className="flex flex-row justify-center">
+                            <div className="flex justify-center gap-2"></div>
                         </div>
+                    </form>
+                    <div className="flex flex-col gap-2 justify-center">
+                        <ButtonStandard type="submit" form="loginForm" onClick={emailLogin}>
+                            <AiOutlineMail className="text-xl" />
+                            Login with Email
+                        </ButtonStandard>
+                        <ButtonStandard
+                            onClick={() => {
+                                signIn(p.providers.google.id);
+                            }}
+                        >
+                            <FcGoogle className="text-xl" />
+                            Login with Google
+                        </ButtonStandard>
+                        <ButtonStandard
+                            onClick={() => {
+                                signIn(p.providers.github.id);
+                            }}
+                        >
+                            <SiGithub className="text-xl" />
+                            Login with Github
+                        </ButtonStandard>
+                        <ButtonStandard
+                            onClick={() => {
+                                signIn(p.providers.slack.id);
+                            }}
+                        >
+                            <SiGithub className="text-xl" />
+                            Login with Slack
+                        </ButtonStandard>
                     </div>
-                </form>
-                <div className="flex flex-row gap-2 justify-center">
-                    <ButtonStandard onClick={() => {}}>
-                        <SiGithub className="text-xl"></SiGithub>Github Login
-                    </ButtonStandard>
-                    <ButtonStandard onClick={() => {}}>
-                        <FcGoogle className="text-xl"></FcGoogle>Google Login
-                    </ButtonStandard>
                 </div>
-                {serverMessage}
+                <div className="text-sm text-accent">
+                    <p className="text-center">{serverMessage}</p>
+                </div>
             </div>
+
             <div className="col-span-0 sm:col-span-2 md:col-span-3 lg:col-span-4 outline-red-500"></div>
         </div>
     );
