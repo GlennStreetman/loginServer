12192024fa9bcd59fe18247b28985f0453374f48
GlennStreetman <glennstreetman@gmail.com>
2022-03-16 22:37:00 +0000

    added logos to login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import LoginHeader from "./../components/loginHeader";
 import Button from "../components/buttonStandard";
-import Link from "next/link";
+import Image from "next/image";
 
 interface user {
     email?: string;
@@ -23,8 +23,14 @@ function mapUser(user) {
     return userMap;
 }
 
+function setDarkStyle(){
+    const mode = localStorage ? localStorage.siteDarkMode : false
+    return mode
+}
+
 const Home: NextPage = () => {
     const [thisSession, setThisSession] = useState<sessionInt>({});
+    const [darkMode] = useState(false)
 
     useEffect(() => {
         async function getSessionObject() {
@@ -33,25 +39,31 @@ const Home: NextPage = () => {
             setThisSession(sessionData);
         }
         getSessionObject();
+        setDarkStyle()
     }, []);
 
     return (
         <div className={styles.container}>
             <LoginHeader />
             <main className={styles.main}>
-                <div>API SESSION STATUS</div>
+                {/* <div>API SESSION STATUS</div>
                 <div>Session expires: {thisSession?.expires ? thisSession.expires : "Not Logged In"}</div>
-                {thisSession?.user ? mapUser(thisSession.user) : "No user"}
+                {thisSession?.user ? mapUser(thisSession.user) : "No user"} */}
 
                         <div>
                             <Button onClick={() => {}}>
-                                <a href='https://blog.gstreet.test'>Blog</a>
+                                <a href='https://blog.gstreet.test'>
+                                { darkMode === true ?
+                                    <Image id="darkOn" src="/gstreetDarkOn.png" alt="logo" height="250px" width="250px" /> :
+                                    <Image id="lightOn" src="/gstreetLightOn.png" alt="logo" height="250px" width="250px" />     
+                                }
+                                </a>
                             </Button>
                         </div>
 
                         <div>
                             <Button onClick={() => {}}>
-                                <a href='https://finndash.gstreet.test'>Finndash</a>
+                                <a href='https://finndash.gstreet.test'> <Image id="finnhubSplash" src="/splashLogo.png" alt="logo" height="250px" width="250px" />  </a>
                             </Button>
                         </div>
 
